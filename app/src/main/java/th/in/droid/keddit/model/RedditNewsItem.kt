7e555ab5d95d9435a756a36153ed7b3ee8fca786
Seven Seconds <sevenseconds@gmail.com>
2017-09-11 00:02:51 +0700

    Implement parcelable

--- app/src/main/java/th/in/droid/keddit/model/RedditNewsItem.kt
+++ app/src/main/java/th/in/droid/keddit/model/RedditNewsItem.kt
@@ -1,7 +1,42 @@
 package th.`in`.droid.keddit.model
 
+import android.os.Parcel
+import android.os.Parcelable
 import th.`in`.droid.keddit.adapter.AdapterConstants
 import th.`in`.droid.keddit.adapter.ViewType
+
+data class RedditNews(
+        val after: String,
+        val before: String,
+        val news: List<RedditNewsItem>
+) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readString(),
+            parcel.createTypedArrayList(RedditNewsItem))
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(after)
+        parcel.writeString(before)
+        parcel.writeTypedList(news)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<RedditNews> {
+        override fun createFromParcel(parcel: Parcel): RedditNews {
+            return RedditNews(parcel)
+        }
+
+        override fun newArray(size: Int): Array<RedditNews?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+}
 
 data class RedditNewsItem(
         val author: String,
@@ -10,6 +45,38 @@ data class RedditNewsItem(
         val created: Long,
         val thumbnail: String,
         val url: String
-) : ViewType {
+) : ViewType, Parcelable {
+
     override fun getViewType(): Int = AdapterConstants.NEWS
+
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readInt(),
+            parcel.readLong(),
+            parcel.readString(),
+            parcel.readString())
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(author)
+        parcel.writeString(title)
+        parcel.writeInt(numComments)
+        parcel.writeLong(created)
+        parcel.writeString(thumbnail)
+        parcel.writeString(url)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<RedditNewsItem> {
+        override fun createFromParcel(parcel: Parcel): RedditNewsItem {
+            return RedditNewsItem(parcel)
+        }
+
+        override fun newArray(size: Int): Array<RedditNewsItem?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
